cf9f8a3e12c4f420d07f53ef3c552dd53b8953ae
Courtney Cotton <ccotton@ussenterprise.local>
2014-01-17 20:28:35 -0800

    Added compliments n' stuff.

--- mushu.py
+++ mushu.py
@@ -8,11 +8,14 @@
 #######################################################
 ### Libraries
 #######################################################
+import logging
 import socket
 import string
 import sys
 import time
+from datetime import date
 from random import choice
+import compliments
 
 #######################################################
 ### VARIABLES
@@ -32,46 +35,60 @@ elif debug == False:
 sfox = [' :Ring ding ding ding ding a ding a ding!\r\n', 
         ' :Wa-pa-pa-pa-pa-pa-pow!\r\n', 
         ' :Chacha-chacha-chacha-chow!\r\n', 
-        ' :Hatee-hatee-hatee-ho!\r\n']
+        ' :Hatee-hatee-hatee-ho!\r\n',
+        " :It's an ancient mystery. You will never know."]
+
+comList = [' :You are a wonderful person.\r\n', 
+            ' :If it was a Zombie Apocalypse and I was a Zombie, I would eat you first.\r\n', 
+            ' :Hey Albert Einste--wait! Nevermind! You are far more brilliant!\r\n', 
+            ' :You are brilliant.\r\n',
+            ' :You smell better than a field full of flowers.',
+            ' :My only regret is that I cannot be you.']
 
 #######################################################
 ### FUNCTIONS
 #######################################################
+
+# Connect variable.
+def connect():
+    global ircsock
+    ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ircsock.connect((server, port))
+    ircsock.send("NICK " + botnick + "\n") 
+    ircsock.send('USER SherlockBot SherlockBot SherlockBot :Sherlock IRC\r\n')
 
 # Responds when asked information on what it does.
 def commands(nick,channel,message):
    
    if message.find('.fox')!=-1:
       ircsock.send('PRIVMSG ' + channel + choice(sfox))
-      #ircsock.send('PRIVMSG ' + channel + " :Ring ding ding ding ding a ding a ding!\r\n ")
    elif message.find('.list')!=-1:
-      ircsock.send('PRIVMSG %s :%s: .fox (DONTUJUDGEME...)\r\n' % (channel,nick))
+      ircsock.send('PRIVMSG %s :%s: .fox | .compliment\r\n' % (channel,nick))
+   elif message.find('.compliment')!=-1:
+      ircsock.send('PRIVMSG ' + channel + choice(comList))
 
 # Responds to server pings.
 def ping(): 
-  ircsock.send("PONG :pingis\n")  
+  ircsock.send('PONG :pingis\n')  
 
 # Sends messages to channel.
 def sendmsg(chan , msg): 
-  ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n") 
+  ircsock.send('PRIVMSG ' + chan + ' :' + msg + '\n') 
 
 # Join a Channel
 def joinchan(chan):
-  ircsock.send("JOIN "+ chan +"\n")
+  ircsock.send('JOIN ' + chan + '\n')
 
 # Responds to Users who say Hello
-def hello(): # This function responds to a user that inputs "Hello Euthumeo"
-  ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
-                  
+def hello(): 
+  ircsock.send('PRIVMSG ' + channel + ' :Hello!\n')
+            
 #######################################################
 ### Code
 #######################################################
 
 ## Connection Section
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ircsock.connect((server, port)) # Here we connect to the server using the port 6667
-ircsock.send("NICK "+ botnick +"\n") # here we actually assign the nick to the bot
-ircsock.send('USER AffixBot AffixBot AffixBot :Affix IRC\r\n')
+connect()
 # Sleep is required because IRC may not be able to keep up with the
 # information being sent, and will fail.
 # Sleeping requires a proper response, and you're able to join channel.
@@ -80,7 +97,10 @@ time.sleep(3)
 joinchan(channel) 
 
 while 1: 
-  ircmsg = ircsock.recv(2048) 
+  ircmsg = ircsock.recv(4096)
+  if len(ircmsg) == 0:
+    print "Disconnected!" # Put a logging statement here instead.
+    connect() 
   ircmsg = ircmsg.strip('\n\r') 
   if ircmsg.find(' PRIVMSG ')!=-1:
      nick=ircmsg.split('!')[0][1:]

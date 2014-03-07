7360265b580186fa68fc3f7fcb58b9cd4b8f7aa3
Courtney C <courtneycotton@berkeley.edu>
2014-03-07 12:14:53 -0800

    Updated to include .quote and .fortune functionality. Also rewrote bot to pull lines from file, rather than an array.

--- mushu.py
+++ mushu.py
@@ -1,28 +1,17 @@
 ########################################################
 #### Python IRC Bot
 #### Created by Courtney Cotton on 1/17/14
-#### Utilized Shellium (http://wiki.shellium.org/) for 
-#### support.
+#### 
 #######################################################
-
-### 
-# TODO
-# Add Logging
-# Add Weather
-# Add Google Search
-# Add Wiki
-# Add a Game (IDK WHICH)
-# Put FoxList/Compliments in a class in another file and import them FOR LEARNINGS!1!@
-
 
 #######################################################
 ### Libraries
 #######################################################
-import logging
 import socket
 import string
 import sys
 import time
+import random
 from random import choice
 
 #######################################################
@@ -32,71 +21,71 @@ from random import choice
 server = "chat.freenode.net" # Server
 port = 6667
 botnick = "mushu" # Your bots nick
-debug = True
+debug = False
 # Debug Mode, Toggle true or false. 
 # Determines connection to channel based on debug variable.
 if debug == True:
-  channel = '#testinglandville'
+  channel = '#aplacefortesting'
 elif debug == False:
-  channel = '#forrealsieschat'
-
-sfox = [' :Ring ding ding ding ding a ding a ding!\r\n', 
-        ' :Wa-pa-pa-pa-pa-pa-pow!\r\n', 
-        ' :Chacha-chacha-chacha-chow!\r\n', 
-        ' :Hatee-hatee-hatee-ho!\r\n',
-        " :It's an ancient mystery. You will never know."]
-
-comList = [' :You are a wonderful person.\r\n', 
-            ' :If it was a Zombie Apocalypse and I was a Zombie, I would eat you first.\r\n', 
-            ' :Hey Albert Einste--wait! Nevermind! You are far more brilliant!\r\n', 
-            ' :You are brilliant.\r\n',
-            ' :You smell better than a field full of flowers.\r\n',
-            ' :My only regret is that I cannot be you.\r\n']
+  channel = '#therealchanneliwant'
 
 #######################################################
 ### FUNCTIONS
 #######################################################
 
-# Connect variable.
-def connect():
-    global ircsock
-    ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ircsock.connect((server, port))
-    ircsock.send("NICK " + botnick + "\n") 
-    ircsock.send('USER SherlockBot SherlockBot SherlockBot :Sherlock IRC\r\n')
-
-# Responds when asked information on what it does.
+# Responds when asked information.
 def commands(nick,channel,message):
-   
-   if message.find('.fox')!=-1:
-      ircsock.send('PRIVMSG ' + channel + choice(sfox))
-   elif message.find('.list')!=-1:
-      ircsock.send('PRIVMSG %s :%s: .fox | .compliment\r\n' % (channel,nick))
-   elif message.find('.compliment')!=-1:
-      ircsock.send('PRIVMSG ' + channel + choice(comList))
+  #Looks for message .fox and reads a random line from files/fox.txt to be sent.
+  if message.find('.fox')!=-1:
+    f = open('files/fox.txt')
+    rFoxPhrase = random.choice(open('files/fox.txt').readlines())
+    ircsock.send('PRIVMSG ' + channel + " :" + rFoxPhrase + "\n")
+    f.close
+  #Looks for message .list and prints out available commands.
+  elif message.find('.list')!=-1:
+    ircsock.send('PRIVMSG %s :%s: .fox | .compliment | .fortune | .quote \r\n' % (channel,nick))
+  #Looks for message .compliment and reads a random line from files/compliments.txt to be sent.
+  elif message.find('.compliment')!=-1:
+    f = open('files/compliments.txt')
+    rComList = random.choice(open('files/compliments.txt').readlines())
+    ircsock.send('PRIVMSG ' + channel + " :" + rComList + "\n")
+    f.close
+  elif message.find('.fortune')!=-1:
+    f = open('files/fortunes.txt')
+    rFortune = random.choice(open('files/fortunes.txt').readlines())
+    ircsock.send('PRIVMSG ' + channel + " :" + rFortune + "\n")
+    f.close
+  elif message.find('.quote')!=-1:
+    f = open('files/quotes.txt')
+    rQuotes = random.choice(open('files/quotes.txt').readlines())
+    ircsock.send('PRIVMSG ' + channel + " :" + rQuotes + "\n")
+    f.close
 
 # Responds to server pings.
 def ping(): 
-  ircsock.send('PONG :pingis\n')  
+  ircsock.send("PONG :pingis\n")  
 
 # Sends messages to channel.
 def sendmsg(chan , msg): 
-  ircsock.send('PRIVMSG ' + chan + ' :' + msg + '\n') 
+  ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n") 
 
 # Join a Channel
 def joinchan(chan):
-  ircsock.send('JOIN ' + chan + '\n')
+  ircsock.send("JOIN "+ chan +"\n")
 
 # Responds to Users who say Hello
-def hello(): 
-  ircsock.send('PRIVMSG ' + channel + ' :Hello!\n')
-            
+def hello(): # This function responds to a user that inputs "Hello Euthumeo"
+  ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
+                  
 #######################################################
 ### Code
 #######################################################
 
 ## Connection Section
-connect()
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ircsock.connect((server, port)) # Here we connect to the server using the port 6667
+ircsock.send("NICK "+ botnick +"\n") # here we actually assign the nick to the bot
+ircsock.send('USER AffixBot AffixBot AffixBot :Affix IRC\r\n')
 # Sleep is required because IRC may not be able to keep up with the
 # information being sent, and will fail.
 # Sleeping requires a proper response, and you're able to join channel.
@@ -105,10 +94,7 @@ time.sleep(3)
 joinchan(channel) 
 
 while 1: 
-  ircmsg = ircsock.recv(4096)
-  if len(ircmsg) == 0:
-    print "Disconnected!" # Put a logging statement here instead.
-    connect() 
+  ircmsg = ircsock.recv(2048) 
   ircmsg = ircmsg.strip('\n\r') 
   if ircmsg.find(' PRIVMSG ')!=-1:
      nick=ircmsg.split('!')[0][1:]
@@ -118,5 +104,5 @@ while 1:
     hello()
 
 # Responds if server pings. Keeps session active.
-  if ircmsg.find("PING :") != -1: 
+  if ircmsg.find("PING :") != -1:
     ping()
